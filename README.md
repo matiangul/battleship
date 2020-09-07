559fd59d44f5da42a77479a22277e7011e1ba735
Mateusz Angulski <mateusz.angulski@assertis.co.uk>
2020-09-07 20:43:54 +0200

    Add React library to declaratively build UI

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ https://nodejs.org/en/
 ## Installation
 
 `npm install`
+
+## Run dev env
+
+`npm run dev`
+
+## Build an app
+
+`npm run build`
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Let's play with React!</h1>, document.getElementById("root"));
